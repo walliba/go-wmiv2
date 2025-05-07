55d1988c1db0f1437094ea5a109d16b0727823c6
walliba <bryce.wallis@compsys.com>
2025-05-07 15:08:06 -0500

    validate MI_Value is not nullptr; needs unit testing

--- internal/mi/value.go
+++ internal/mi/value.go
@@ -14,40 +14,44 @@ type Value struct {
 }
 
 func (v *Value) As(t Type) any {
+
+	ptr := v.GetPointer()
+
+	if ptr == nil {
+		panic("<value pointer is null>")
+	}
+
 	switch t {
 	case MI_BOOLEAN:
 		// 0
-		return *(*bool)(v.GetPointer())
+		return *(*bool)(ptr)
 	case MI_UINT8:
 		// 1
-		return *(*uint8)(v.GetPointer())
+		return *(*uint8)(ptr)
 	case MI_SINT8:
 		// 2
-		return *(*int8)(v.GetPointer())
+		return *(*int8)(ptr)
 	case MI_UINT16:
 		// 3
-		return *(*uint16)(v.GetPointer())
+		return *(*uint16)(ptr)
 	case MI_SINT16:
 		// 4
-		return *(*int16)(v.GetPointer())
+		return *(*int16)(ptr)
 	case MI_UINT32:
 		// 5
-		return *(*uint32)(v.GetPointer())
+		return *(*uint32)(ptr)
 	case MI_SINT32:
 		// 6
-		return *(*int32)(v.GetPointer())
+		return *(*int32)(ptr)
 	case MI_UINT64:
 		// 7
-		return *(*uint64)(v.GetPointer())
+		return *(*uint64)(ptr)
 	case MI_DATETIME:
 		// 12
-		return *(*types.DateTime)(v.GetPointer())
+		return *(*types.DateTime)(ptr)
 	case MI_STRING:
 		// 13
-		ptr := v.GetPointer()
-		if ptr != nil {
-			return windows.UTF16PtrToString(*(**uint16)((ptr)))
-		}
+		return windows.UTF16PtrToString(*(**uint16)((ptr)))
 	default:
 		fmt.Fprintf(os.Stderr, "<unsupported type: %d>\n", t)
 	}
