7a944eb2f26e53dc1517988894e2b854268225fb
walliba <bryce.wallis@compsys.com>
2025-05-07 12:34:47 -0500

    add MI_Value->MI_DateTime translation

--- internal/mi/value.go
+++ internal/mi/value.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"unsafe"
 
+	"github.com/walliba/go-wmiv2/internal/mi/types"
 	"golang.org/x/sys/windows"
 )
 
@@ -15,26 +16,40 @@ type Value struct {
 func (v *Value) As(t Type) any {
 	switch t {
 	case MI_BOOLEAN:
+		// 0
 		return *(*bool)(v.GetPointer())
 	case MI_UINT8:
+		// 1
 		return *(*uint8)(v.GetPointer())
 	case MI_SINT8:
+		// 2
 		return *(*int8)(v.GetPointer())
 	case MI_UINT16:
+		// 3
 		return *(*uint16)(v.GetPointer())
 	case MI_SINT16:
+		// 4
 		return *(*int16)(v.GetPointer())
 	case MI_UINT32:
+		// 5
 		return *(*uint32)(v.GetPointer())
 	case MI_SINT32:
+		// 6
 		return *(*int32)(v.GetPointer())
+	case MI_UINT64:
+		// 7
+		return *(*uint64)(v.GetPointer())
+	case MI_DATETIME:
+		// 12
+		return *(*types.DateTime)(v.GetPointer())
 	case MI_STRING:
+		// 13
 		ptr := v.GetPointer()
 		if ptr != nil {
 			return windows.UTF16PtrToString((*uint16)(*(*unsafe.Pointer)(ptr)))
 		}
 	default:
-		fmt.Fprintf(os.Stderr, "<unsupported type: %d>", t)
+		fmt.Fprintf(os.Stderr, "<unsupported type: %d>\n", t)
 	}
 
 	return nil
