276b8e83d76375823e2e2a61ca78617e92292325
walliba <bryce.wallis@compsys.com>
2025-05-07 16:22:23 -0500

    use loop in Query example

--- example/query.go
+++ example/query.go
@@ -14,8 +14,9 @@ func main() {
 
 	result := client.Query("select name, processid from win32_process where processid = 9880 or processid = 10904")
 
-	fmt.Printf("Process '%s' with PID %d\n", result[0]["Name"], result[0]["ProcessId"])
-	fmt.Printf("Process '%s' with PID %d\n", result[1]["Name"], result[1]["ProcessId"])
+	for i := range result {
+		fmt.Printf("Process '%s' with PID %d\n", result[i]["Name"], result[i]["ProcessId"])
+	}
 
 	os.Exit(0)
 }
