7e4aeb4772589eaa66f96d7466bfb9c1733c4320
walliba <bryce.wallis@compsys.com>
2025-05-15 01:12:55 -0500

    feat: partially implement MI_Class
    - removed dynamicstructs as it is not necessary

--- application.go
+++ application.go
@@ -8,7 +8,7 @@ import (
 
 type miApplication struct {
 	raw      *mi.Application
-	sessions []Session
+	sessions []*mi.Session
 }
 
 func (app *miApplication) NewSession(destination string) (Session, error) {
@@ -18,20 +18,23 @@ func (app *miApplication) NewSession(destination string) (Session, error) {
 		return nil, fmt.Errorf("error creating session")
 	}
 
+	app.sessions = append(app.sessions, session)
+
 	return &miSession{raw: session}, nil
 }
 
 func (app *miApplication) Close() error {
 	// // This feels like a band-aid solution
-	// if sessionCount := len(app.sessions); sessionCount > 0 {
-	// 	for i := range app.sessions {
-	// 		// TODO: handle failure (err)
-	// 		app.sessions[i].Close()
-	// 	}
+	if sessionCount := len(app.sessions); sessionCount > 0 {
+		for i := range app.sessions {
+			// TODO: handle failure (err)
+			app.sessions[i].Close()
+			app.sessions[i] = nil
+		}
 
-	// 	// allow slice to be garbage collected
-	// 	app.sessions = nil
-	// }
+		// allow slice to be garbage collected
+		app.sessions = nil
+	}
 
 	result := app.raw.Close()
 	if result != mi.RESULT_OK {
