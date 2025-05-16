ef5c006203884b9ac05dd9d661721ff0dfa4b261
walliba <bryce.wallis@compsys.com>
2025-05-16 13:54:53 -0500

    chore: scaffold MI_Session struct

--- internal/mi/mi_session.go
+++ internal/mi/mi_session.go
@@ -8,30 +8,65 @@ import (
 type Session struct {
 	reserved1 uint64
 	reserved2 int64 // ptrdiff_t
-	ft        *SessionFT
+	ft        *sessionFT
 }
 
-type SessionFT struct {
-	Close               uintptr
-	GetApplication      uintptr
-	GetInstance         uintptr
-	ModifyInstance      uintptr
-	CreateInstance      uintptr
-	DeleteInstance      uintptr
-	Invoke              uintptr
-	EnumerateInstances  uintptr
-	QueryInstances      uintptr
-	AssociatorInstances uintptr
-	ReferenceInstances  uintptr
-	Subscribe           uintptr
-	GetClass            uintptr
-	EnumerateClasses    uintptr
-	TestConnection      uintptr
+type sessionFT struct {
+	close               uintptr
+	getApplication      uintptr
+	getInstance         uintptr
+	modifyInstance      uintptr
+	createInstance      uintptr
+	deleteInstance      uintptr
+	invoke              uintptr
+	enumerateInstances  uintptr
+	queryInstances      uintptr
+	associatorInstances uintptr
+	referenceInstances  uintptr
+	subscribe           uintptr
+	getClass            uintptr
+	enumerateClasses    uintptr
+	testConnection      uintptr
 }
 
 func (s *Session) Close() Result {
-	r0, _, _ := syscall.SyscallN(s.ft.Close, uintptr(unsafe.Pointer(s)), 0, uintptr(0))
+	r0, _, _ := syscall.SyscallN(s.ft.close, uintptr(unsafe.Pointer(s)), 0, uintptr(0))
 	return Result(r0)
+}
+
+func (s *Session) GetApplication() { panic("not implemented") }
+
+func (s *Session) GetInstance() { panic("not implemented") }
+
+func (s *Session) ModifyInstance() { panic("not implemented") }
+
+func (s *Session) CreateInstance() { panic("not implemented") }
+
+func (s *Session) DeleteInstance() { panic("not implemented") }
+
+func (s *Session) Invoke() { panic("not implemented") }
+
+func (s *Session) EnumerateInstances(namespace string, class string) *Operation {
+	// var namespace = "root\\cimv2"
+	// var class = "Win32_Process"
+
+	ns, _ := syscall.UTF16PtrFromString(namespace)
+	c, _ := syscall.UTF16PtrFromString(class)
+
+	var miOperation = MI_OPERATION_NULL
+
+	_, _, _ = syscall.SyscallN(s.ft.enumerateInstances,
+		uintptr(unsafe.Pointer(s)),
+		0,
+		0,
+		uintptr(unsafe.Pointer(ns)),
+		uintptr(unsafe.Pointer(c)),
+		0,
+		0,
+		uintptr(unsafe.Pointer(&miOperation)),
+	)
+
+	return &miOperation
 }
 
 func (s *Session) QueryInstances(namespace string, query string) *Operation {
@@ -41,7 +76,7 @@ func (s *Session) QueryInstances(namespace string, query string) *Operation {
 
 	var miOperation = MI_OPERATION_NULL
 
-	_, _, _ = syscall.SyscallN(s.ft.QueryInstances,
+	_, _, _ = syscall.SyscallN(s.ft.queryInstances,
 		uintptr(unsafe.Pointer(s)),            // Session
 		0,                                     // Flags
 		uintptr(0),                            // Options
@@ -56,28 +91,11 @@ func (s *Session) QueryInstances(namespace string, query string) *Operation {
 
 }
 
-func (s *Session) EnumerateInstances(namespace string, class string) *Operation {
-	// var namespace = "root\\cimv2"
-	// var class = "Win32_Process"
+func (s *Session) AssociatorInstances() { panic("not implemented") }
 
-	ns, _ := syscall.UTF16PtrFromString(namespace)
-	c, _ := syscall.UTF16PtrFromString(class)
+func (s *Session) ReferenceInstances() { panic("not implemented") }
 
-	var miOperation = MI_OPERATION_NULL
-
-	_, _, _ = syscall.SyscallN(s.ft.EnumerateInstances,
-		uintptr(unsafe.Pointer(s)),
-		0,
-		0,
-		uintptr(unsafe.Pointer(ns)),
-		uintptr(unsafe.Pointer(c)),
-		0,
-		0,
-		uintptr(unsafe.Pointer(&miOperation)),
-	)
-
-	return &miOperation
-}
+func (s *Session) Subscribe() { panic("not implemented") }
 
 /*
 GetClass signature
@@ -93,7 +111,7 @@ func (s *Session) GetClass(namespaceName string, className string) *Operation {
 
 	operation := new(Operation)
 
-	_, _, _ = syscall.SyscallN(s.ft.GetClass,
+	_, _, _ = syscall.SyscallN(s.ft.getClass,
 		uintptr(unsafe.Pointer(s)),
 		uintptr(0),
 		uintptr(0),
@@ -114,7 +132,7 @@ func (s *Session) EnumerateClasses(namespace string, classNamesOnly bool) *Opera
 
 	operation := new(Operation)
 
-	_, _, _ = syscall.SyscallN(s.ft.EnumerateClasses,
+	_, _, _ = syscall.SyscallN(s.ft.enumerateClasses,
 		uintptr(unsafe.Pointer(s)),
 		0,
 		0,
@@ -127,3 +145,5 @@ func (s *Session) EnumerateClasses(namespace string, classNamesOnly bool) *Opera
 
 	return operation
 }
+
+func (s *Session) TestConnection() { panic("not implemented") }
