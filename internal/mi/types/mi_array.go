730a9b1d93a96ab9705cfae41a63b610c77d7511
walliba <bryce.wallis@compsys.com>
2025-05-16 11:21:39 -0500

    refactor: use idiomatic Go for mi_array
    - updated methods to pointer receivers for efficiency
    - using unsafe.Slice instead of reinterpret cast method

--- internal/mi/types/mi_array.go
+++ internal/mi/types/mi_array.go
@@ -14,29 +14,27 @@ type Array[T any] struct {
 	size uint32
 }
 
-func (x Array[T]) Pointer() unsafe.Pointer {
+func (x *Array[T]) Pointer() unsafe.Pointer {
 	return unsafe.Pointer(x.data)
 }
 
-func (x Array[T]) Size() uint32 {
+func (x *Array[T]) Size() uint32 {
 	return x.size
 }
 
-func (x Array[T]) MakeSlice() []T {
+func (x *Array[T]) MakeSlice() []T {
 	return genericSlice[T](x)
 }
 
 func genericSlice[T any](arr MIArray) []T {
-	var size = arr.Size()
-	const max = 1 << 20
+	size := arr.Size()
 
-	// reinterpret array to slice
-	mem := (*[max]T)(arr.Pointer())[:size:size]
+	// set slice backing array
+	mem := unsafe.Slice((*T)(arr.Pointer()), size)
 
-	// make a copy of the slice (C array) since mi.dll will free the memory after iteration
+	// make a copy of the slice to avoid use-after-free
 	r := make([]T, size)
 	_ = copy(r, mem)
 
-	// return copy
 	return r
 }
